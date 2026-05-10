0cc1823c4db4f67a2dc9626984c110d8843b199d
Yunan Rai <yunanrai433@gmail.com>
2026-05-10 11:48:16 +0545

    feat: install tailwind css and setup initial routing in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import OnBoarding from "./pages/OnBoarding"
+import Profile from "./pages/Profile"
+import Auth from "./pages/Auth"
+import Account from "./pages/Account"
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+    <Routes>
+      <Route index element={<Home/>}/>
+      <Route path="/onboarding" element={<OnBoarding/>}/>
+      <Route path="/profile" element={<Profile/>}/>
+      <Route path="/auth/:pathname" element={<Auth/>}/>
+      <Route path="/account/:pathname" element={<Account/>}/>
+
+    </Routes>
+    </BrowserRouter>
   )
 }
 
