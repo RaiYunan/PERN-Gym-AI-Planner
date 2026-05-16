a75de825941ba0a25a079e0ce668bb95dbc59e5f
Yunan Rai <yunanrai433@gmail.com>
2026-05-16 23:54:40 +0545

    feat: added and improved navbar design

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,7 +9,7 @@ import Navbar from "./components/layout/Navbar";
 const App = () => {
   return (
     <BrowserRouter>
-      <div className="min-h-screen flex flex-col bg-slate-900">
+      <div className="min-h-screen flex flex-col bg-black">
         <Navbar/>
         <main className="flex-1">
           <Routes>
--- client/src/components/layout/Navbar.tsx
+++ client/src/components/layout/Navbar.tsx
@@ -4,15 +4,20 @@ import { Button } from "../ui/button";
 
 const Navbar = () => {
   return (
-    <header className="fixed top-0 left-0 right-0 z-50 border-b border-border border-b-black backdrop-blur-md">
-      <div className="max-w-6xl mx-auto px-6 h-16 flex items-center justify-between">
+    <header className="fixed top-0 left-0 right-0 z-50 border-b  border-border backdrop-blur-md bg bg-black">
+      <div className="max-w-full px-8 h-16 flex items-center justify-between">
         <Link to="/" className="flex items-center gap-2 text-foreground">
           <Dumbbell className="w-6 h-6 text-accent" />
-          <span className="font-semibold text-lg">GymAI</span>
+          <span className="font-semibold text-lg text-accent-hover">GymAI</span>
         </Link>
-        <nav>
+        <nav className="gap-2 flex ">
           <>
-            <Link to="/auth/sign-in"><Button className="bg-white text-black">Sign in</Button></Link>
+            <Link to="/auth/sign-in">
+              <Button className="text-white px-6 py-4" variant="ghost" >Sign in</Button>
+            </Link>
+             <Link to="/auth/signup">
+              <Button className="text-black bg-accent-hover px-3 py-4">Sign up</Button>
+            </Link>
           </>
         </nav>
       </div>
