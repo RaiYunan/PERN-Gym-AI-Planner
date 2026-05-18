da6e312c8fc85ae7851a5f3d4431d9972e2f559d
Yunan Rai <yunanrai433@gmail.com>
2026-05-18 22:40:39 +0545

    feat(auth): integrate Neon Auth and resolve provider type mismatch
    - Configure NeonAuthUIProvider with authClient setup
    - Resolve TypeScript beta definition mismatch by casting authClient via unknown
    - Fix ESLint no-explicit-any violation

--- client/src/components/layout/Navbar.tsx
+++ client/src/components/layout/Navbar.tsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { Button } from "../ui/button";
 
 const Navbar = () => {
+  const user = true;
   return (
     <header className="fixed top-0 left-0 right-0 z-50 border-b  border-border backdrop-blur-md bg bg-black">
       <div className="max-w-full px-8 h-16 flex items-center justify-between">
@@ -11,14 +12,28 @@ const Navbar = () => {
           <span className="font-semibold text-lg text-accent-hover">GymAI</span>
         </Link>
         <nav className="gap-2 flex ">
-          <>
-            <Link to="/auth/sign-in">
-              <Button className="text-white px-6 py-4" variant="ghost" >Sign in</Button>
-            </Link>
-             <Link to="/auth/signup">
-              <Button className="text-black bg-accent-hover px-3 py-4">Sign up</Button>
-            </Link>
-          </>
+          {user ? (
+            <>
+              <Link to="/auth/sign-in">
+                <Button className="text-white px-6 py-4 cursor-pointer" variant="ghost">
+                  My Plan
+                </Button>
+              </Link>
+            </>
+          ) : (
+            <>
+              <Link to="/auth/sign-in">
+                <Button className="text-white px-6 py-4" variant="ghost">
+                  Sign in
+                </Button>
+              </Link>
+              <Link to="/auth/signup">
+                <Button className="text-black bg-accent-hover px-3 py-4">
+                  Sign up
+                </Button>
+              </Link>
+            </>
+          )}
         </nav>
       </div>
     </header>
